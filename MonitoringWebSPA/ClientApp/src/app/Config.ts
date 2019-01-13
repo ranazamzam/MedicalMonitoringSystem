fe959fe1c1880e52086fbf174b48cc5850199a9d
rana zamzam <rana.zamzam@gmail.com>
2019-01-13 20:54:50 +0300

    update config signal r not working on cloud

--- MonitoringWebSPA/ClientApp/src/app/Config.ts
+++ MonitoringWebSPA/ClientApp/src/app/Config.ts
@@ -6,7 +6,8 @@ export let Config = {
     //APIGatewayEvents: 'http://localhost:19081/MedicalBookingSystem/MedicalBookingSystem.APIGateway/Events'
     AzureSignalrUrl: 'https://medicalbookingmonitoringsystemfunctionapp.azurewebsites.net/api/',
     APIGateway: 'http://medicalsystemmonitoring.northeurope.cloudapp.azure.com:8081',
-    APIGatewaySignalR: 'http://medicalsystemmonitoring.northeurope.cloudapp.azure.com:8081/eventsGenerator',
+    APIGatewaySignalR: 'http://medicalsystemmonitoring.northeurope.cloudapp.azure.com:19081/MedicalBookingSystem/MedicalBookingSystem.APIGateway/eventsGenerator',
+    //APIGatewaySignalR: 'http://medicalsystemmonitoring.northeurope.cloudapp.azure.com:8081/eventsGenerator',
     APIGatewayEvents: 'http://medicalsystemmonitoring.northeurope.cloudapp.azure.com:8081/Events'
   }
 };
